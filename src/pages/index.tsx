e74b8eeecb42d282196eb79539093be506c89e94
skrt-dev <jacobschwantes@outlook.com>
2022-07-06 17:29:52 -0500

    front page components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,66 @@ import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { ChevronRightIcon, StarIcon } from "@heroicons/react/solid";
+import { InboxIcon, SparklesIcon } from '@heroicons/react/outline'
+import {
+  CloudUploadIcon,
+  CogIcon,
+  LockClosedIcon,
+  RefreshIcon,
+  ServerIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/outline'
+import Announcement from "@components/Announcement";
+
+const features = [
+  {
+    name: 'Push to Deploy',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: CloudUploadIcon,
+  },
+  {
+    name: 'SSL Certificates',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple Queues',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: RefreshIcon,
+  },
+  {
+    name: 'Advanced Security',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: 'Powerful API',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: CogIcon,
+  },
+  {
+    name: 'Database Backups',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+  },
+]
 const Home: NextPage = () => {
   return (
-    <div>
-      <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
-        <div className="overflow-hidden lg:relative lg:py-36">
+    <div className="">
+     {/* <div className="grid-effect min-h-[300px] px-4 md:p-5 h-full border border-gray-100 rounded-xl overflow-hidden flex items-center justify-center">
+
+     </div> */}
+      <div className="bg-white pb-8 sm:pb-12 lg:pb-12 grid-effect">
+        <div className="overflow-hidden lg:relative lg:py-48">
           <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
             <div>
               <div className="">
                 <div>
                   <a href="#" className="inline-flex space-x-4">
-                    <span className="rounded bg-indigo-50 px-2.5 py-1 text-xs font-semibold text-indigo-600 tracking-wide uppercase">
+                    <span className="rounded bg-blue-50 px-2.5 py-1 text-xs font-semibold text-blue-600 tracking-wide uppercase">
                       What's new
                     </span>
-                    <span className="inline-flex items-center text-sm font-medium text-indigo-600 space-x-1">
+                    <span className="inline-flex items-center text-sm font-medium text-blue-600 space-x-1">
                       <span>Just shipped version 0.1.0</span>
                       <ChevronRightIcon
                         className="h-5 w-5"
@@ -47,14 +93,14 @@ const Home: NextPage = () => {
                     <input
                       id="hero-email"
                       type="email"
-                      className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                      className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                       placeholder="Enter your email"
                     />
                   </div>
                   <div className="mt-4 sm:mt-0 sm:ml-3">
                     <button
                       type="submit"
-                      className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                      className="block w-full rounded-md border border-transparent px-5 py-3 bg-blue-600 text-base font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:px-10"
                     >
                       Notify me
                     </button>
@@ -89,7 +135,7 @@ const Home: NextPage = () => {
                         Rated 5 stars
                       </span>{" "}
                       by over{" "}
-                      <span className="font-medium text-indigo-600">
+                      <span className="font-medium text-blue-600">
                         500 beta users
                       </span>
                     </div>
@@ -147,25 +193,191 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
+      <div className="relative bg-gray-50 pt-16 overflow-hidden sm:pt-24 lg:pt-32">
+      <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+        <div>
+          <h2 className="text-base font-semibold tracking-wider text-blue-600 uppercase">Serverless</h2>
+          <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+            No server? No problem.
+          </p>
+          <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+            Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
+            malesuada. Eleifend condimentum id viverra nulla.
+          </p>
+        </div>
+        <div className="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
+          <img
+            className="rounded-lg shadow-xl ring-1 ring-black ring-opacity-5"
+            src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+    <div className="relative bg-white py-16 sm:py-24 lg:py-32">
+      <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-base font-semibold uppercase tracking-wider text-blue-600">Deploy faster</h2>
+        <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Everything you need to deploy your app
+        </p>
+        <p className="mx-auto mt-5 max-w-prose text-xl text-gray-500">
+          Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
+          malesuada. Eleifend condimentum id viverra nulla.
+        </p>
+        <div className="mt-12">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {features.map((feature) => (
+              <div key={feature.name} className="pt-6">
+                <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center rounded-md bg-black p-3 shadow-lg">
+                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">{feature.name}</h3>
+                    <p className="mt-5 text-base text-gray-500">{feature.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="relative bg-white pt-16 pb-32 overflow-hidden">
+      <div className="relative">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+            <div>
+              <div>
+                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-blue-600">
+                  <InboxIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </span>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  Stay on top of customer support
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
+                  porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus
+                  viverra dui tellus ornare pharetra.
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="#"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700"
+                  >
+                    Get started
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 border-t border-gray-200 pt-6">
+              <blockquote>
+                <div>
+                  <p className="text-base text-gray-500">
+                    &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean curabitur
+                    donec aliquet. Mi venenatis in euismod ut.&rdquo;
+                  </p>
+                </div>
+                <footer className="mt-3">
+                  <div className="flex items-center space-x-3">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-6 w-6 rounded-full"
+                        src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                        alt=""
+                      />
+                    </div>
+                    <div className="text-base font-medium text-gray-700">Marcia Hill, Digital Marketing Manager</div>
+                  </div>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0">
+            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                src="https://res.cloudinary.com/dxgbphyhg/image/upload/v1656898812/tupbjwxcmrfnbsjt7isf.png"
+                alt="Inbox user interface"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+            <div>
+              <div>
+                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-blue-600">
+                  <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </span>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  Better understand your customers
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
+                  porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus
+                  viverra dui tellus ornare pharetra.
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="#"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700"
+                  >
+                    Get started
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                src="https://res.cloudinary.com/dxgbphyhg/image/upload/v1656898812/tupbjwxcmrfnbsjt7isf.png"
+                alt="Customer profile user interface"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div className="bg-gray-50">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">Ready to dive in?</span>
+          <span className="block text-blue-600">Start your free trial today.</span>
+        </h2>
+        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+          <div className="inline-flex rounded-md shadow">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+            >
+              Get started
+            </a>
+          </div>
+          <div className="ml-3 inline-flex rounded-md shadow">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-blue-50"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
-
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 
 export default Home;
