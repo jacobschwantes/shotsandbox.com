02f70380c0d87841169abc4ec14e957bc52872d7
skrt-dev <jacobschwantes@outlook.com>
2022-08-26 10:35:17 -0500

    style: change footer, comment out announcment

--- src/common/layouts/default.tsx
+++ src/common/layouts/default.tsx
@@ -1,15 +1,17 @@
 import { NextPage } from "next";
 import { Header, Footer, Announcement } from "@components/index";
 import { useState } from "react";
-
+import { Footer2 } from "@components/Footer/Footer";
 const Layout: NextPage = ({ children }) => {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(true);
   return (
-    <div className="font-inter">
-       {open && <Announcement setOpen={setOpen} />}
-      <Header />
+    <div className="font-inter h-screen overflow-y-auto dark:bg-black">
+      {/* {open && <Announcement setOpen={setOpen} />} */}
+    
+        <Header />
+  
       {children}
-      <Footer/>
+      <Footer2 />
     </div>
   );
 };
