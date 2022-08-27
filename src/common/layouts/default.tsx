f4c882f2150702c08cbc6d8d5ac0b786c65a04b8
jsch <jacobschwantes@outlook.com>
2022-08-26 20:15:32 -0500

    feat: head title tag

--- src/common/layouts/default.tsx
+++ src/common/layouts/default.tsx
@@ -2,10 +2,16 @@ import { NextPage } from "next";
 import { Header, Footer, Announcement } from "@components/index";
 import { useState } from "react";
 import { Footer2 } from "@components/Footer/Footer";
+import Head from "next/head";
 const Layout: NextPage = ({ children }) => {
   const [open, setOpen] = useState(true);
   return (
     <div className="font-inter h-screen overflow-y-auto dark:bg-black">
+      <Head>
+        <title>
+          Screenshotify &ndash; Powerful marketing tool
+        </title>
+      </Head>
       {/* {open && <Announcement setOpen={setOpen} />} */}
     
         <Header />
