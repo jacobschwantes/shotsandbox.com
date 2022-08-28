f63ab3fb4a6b3cf3cace63279954ea1521223cfd
skrt-dev <jacobschwantes@outlook.com>
2022-08-28 17:44:06 -0500

    style: colors, add year/month pricing

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,102 +1,109 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { CheckIcon } from '@heroicons/react/solid'
-
+import { CheckIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import { Tabs } from "@components/index";
 const tiers = [
   {
-    name: 'Hobby',
-    href: '#',
-    priceMonthly: 12,
-    description: 'All the basics for starting a new business',
-    includedFeatures: ['Potenti felis, in cras at at ligula nunc.', 'Orci neque eget pellentesque.'],
-  },
-  {
-    name: 'Freelancer',
-    href: '#',
-    priceMonthly: 24,
-    description: 'All the basics for starting a new business',
+    name: "Free",
+    href: "#",
+    priceMonthly: 0,
+    priceYearly: 0,
+    description: "All the basics for starting a new business",
     includedFeatures: [
-      'Potenti felis, in cras at at ligula nunc. ',
-      'Orci neque eget pellentesque.',
-      'Donec mauris sit in eu tincidunt etiam.',
+      "Potenti felis, in cras at at ligula nunc.",
+      "Orci neque eget pellentesque.",
     ],
   },
   {
-    name: 'Startup',
-    href: '#',
-    priceMonthly: 32,
-    description: 'All the basics for starting a new business',
+    name: "Pro",
+    href: "#",
+    priceMonthly: 8,
+    priceYearly: 50,
+    description: "All the basics for starting a new business",
     includedFeatures: [
-      'Potenti felis, in cras at at ligula nunc. ',
-      'Orci neque eget pellentesque.',
-      'Donec mauris sit in eu tincidunt etiam.',
-      'Faucibus volutpat magna.',
+      "Potenti felis, in cras at at ligula nunc. ",
+      "Orci neque eget pellentesque.",
+      "Donec mauris sit in eu tincidunt etiam.",
     ],
   },
   {
-    name: 'Enterprise',
-    href: '#',
-    priceMonthly: 48,
-    description: 'All the basics for starting a new business',
+    name: "Pro+",
+    href: "#",
+    priceMonthly: 15,
+    priceYearly: 80,
+    description: "All the basics for starting a new business",
     includedFeatures: [
-      'Potenti felis, in cras at at ligula nunc. ',
-      'Orci neque eget pellentesque.',
-      'Donec mauris sit in eu tincidunt etiam.',
-      'Faucibus volutpat magna.',
-      'Id sed tellus in varius quisque.',
-      'Risus egestas faucibus.',
-      'Risus cursus ullamcorper.',
+      "Potenti felis, in cras at at ligula nunc. ",
+      "Orci neque eget pellentesque.",
+      "Donec mauris sit in eu tincidunt etiam.",
+      "Faucibus volutpat magna.",
     ],
   },
-]
+];
 
 export default function Example() {
+  const [selected, setSelected] = useState("yearly");
   return (
-    <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:px-8">
+    <div className="bg-white dark:bg-black">
+      <div className="max-w-7xl mx-auto py-36 px-4 sm:px-6 lg:px-8 space-y-12">
         <div className="sm:flex sm:flex-col sm:align-center">
-          <h1 className="text-5xl font-extrabold text-gray-900 sm:text-center">Pricing Plans</h1>
-          <p className="mt-5 text-xl text-gray-500 sm:text-center">
-            Start building for free, then add a site plan to go live. Account plans unlock additional features.
+          <h1 className="text-5xl font-bold text-gray-900 sm:text-center dark:text-zinc-50">
+            Pricing Plans
+          </h1>
+          <p className="mt-5 text-xl text-gray-500 sm:text-center dark:text-zinc-400">
+            Start building for free, then add a site plan to go live. Account
+            plans unlock additional features.
           </p>
-          <div className="relative self-center mt-6 bg-gray-100 rounded-lg p-0.5 flex sm:mt-8">
-            <button
-              type="button"
-              className="relative w-1/2 bg-white border-gray-200 rounded-md shadow-sm py-2 text-sm font-medium text-gray-900 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-blue-500 focus:z-10 sm:w-auto sm:px-8"
-            >
-              Monthly billing
-            </button>
-            <button
-              type="button"
-              className="ml-0.5 relative w-1/2 border border-transparent rounded-md py-2 text-sm font-medium text-gray-700 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-blue-500 focus:z-10 sm:w-auto sm:px-8"
-            >
-              Yearly billing
-            </button>
-          </div>
         </div>
-        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+        <div className="max-w-xl mx-auto w-full">
+          <Tabs
+            selected={selected}
+            setSelected={setSelected}
+            tabs={["yearly billing", "monthly billing"]}
+          />
+        </div>
+        <div className="space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
           {tiers.map((tier) => (
-            <div key={tier.name} className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200">
+            <div
+              key={tier.name}
+              className="border border-gray-200 dark:border-zinc-800 rounded-lg shadow-sm divide-y divide-gray-200 dark:divide-zinc-800"
+            >
               <div className="p-6">
-                <h2 className="text-lg leading-6 font-medium text-gray-900">{tier.name}</h2>
-                <p className="mt-4 text-sm text-gray-500">{tier.description}</p>
+                <h2 className="text-lg leading-6 font-medium text-gray-900 dark:text-zinc-100">
+                  {tier.name}
+                </h2>
+                <p className="mt-4 text-sm text-gray-500 dark:text-zinc-400">
+                  {tier.description}
+                </p>
                 <p className="mt-8">
-                  <span className="text-4xl font-extrabold text-gray-900">${tier.priceMonthly}</span>{' '}
-                  <span className="text-base font-medium text-gray-500">/mo</span>
+                  <span className="text-4xl font-bold dark:text-zinc-100 text-gray-900">
+                    ${selected === 'monthly billing' ? tier.priceMonthly : tier.priceYearly}
+                  </span>{" "}
+                  <span className="text-base font-medium text-gray-500">
+                    /{selected === 'monthly billing' ? 'mo' : 'yr'}
+                  </span>
                 </p>
                 <a
                   href={tier.href}
-                  className="mt-8 block w-full bg-gray-800 border border-gray-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-gray-900"
+                  className="mt-8 block w-full bg-blue-600 border border-zinc-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-blue-700"
                 >
                   Buy {tier.name}
                 </a>
               </div>
               <div className="pt-6 pb-8 px-6">
-                <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">What's included</h3>
+                <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
+                  What's included
+                </h3>
                 <ul role="list" className="mt-6 space-y-4">
                   {tier.includedFeatures.map((feature) => (
                     <li key={feature} className="flex space-x-3">
-                      <CheckIcon className="flex-shrink-0 h-5 w-5 text-green-500" aria-hidden="true" />
-                      <span className="text-sm text-gray-500">{feature}</span>
+                      <CheckIcon
+                        className="flex-shrink-0 h-5 w-5 text-blue-500"
+                        aria-hidden="true"
+                      />
+                      <span className="text-sm text-gray-500 dark:text-zinc-400">
+                        {feature}
+                      </span>
                     </li>
                   ))}
                 </ul>
@@ -106,5 +113,5 @@ export default function Example() {
         </div>
       </div>
     </div>
-  )
+  );
 }
