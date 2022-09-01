38adc58c610e7a59bc150bd9de19d682d8636886
skrt-dev <jacobschwantes@outlook.com>
2022-09-01 16:50:12 -0500

    fix: add layouts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/common/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
