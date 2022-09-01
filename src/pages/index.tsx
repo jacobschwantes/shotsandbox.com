777340baba25bb6e85f4cf34ae6823abcd88e251
skrt-dev <jacobschwantes@outlook.com>
2022-09-01 16:50:27 -0500

    feat: update links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -238,13 +238,13 @@ const Home: NextPage = () => {
           <h2 className="text-3xl font-bold text-gray-900  dark:text-white sm:text-4xl">
             <span className="block">Ready to dive in?</span>
             <span className="block text-blue-600">
-              Start your free trial today.
+              Start using for free.
             </span>
           </h2>
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md shadow">
               <a
-                href="#"
+                href="https://app.screenshotify.io"
                 className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
               >
                 Get started
