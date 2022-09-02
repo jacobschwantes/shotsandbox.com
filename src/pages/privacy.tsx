a184d087da3a61d3fde4e2f441a17db53f6530f5
skrt-dev <jacobschwantes@outlook.com>
2022-09-02 11:43:41 -0500

    feat: add privacy and terms

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,20 @@
+import Script from "next/script";
+const Privacy = () => (
+  <div className="w-full h-full bg-black">
+    <div
+      className="bg-black max-w-6xl mx-auto pt-16"
+      //@ts-ignore
+      name="termly-embed"
+      data-id="1fa34cc3-1c4f-4c9a-aac1-5beb22eaef48"
+      data-type="iframe"
+    ></div>
+    <Script type="text/javascript">{`(function(d, s, id) {
+  var js, tjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://app.termly.io/embed-policy.min.js";
+  tjs.parentNode.insertBefore(js, tjs);
+}(document, 'script', 'termly-jssdk'));`}</Script>
+  </div>
+);
+export default Privacy;
---
+++ src/pages/terms.tsx
@@ -0,0 +1,20 @@
+import Script from "next/script";
+const Terms = () => (
+  <div className="w-full h-full bg-black">
+    <div
+      className="bg-black max-w-6xl mx-auto pt-16"
+      //@ts-ignore
+      name="termly-embed"
+      data-id="d84722d7-35f6-41f0-a416-bdb04899789c"
+      data-type="iframe"
+    ></div>
+    <Script type="text/javascript">{`(function(d, s, id) {
+  var js, tjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://app.termly.io/embed-policy.min.js";
+  tjs.parentNode.insertBefore(js, tjs);
+}(document, 'script', 'termly-jssdk'));`}</Script>
+  </div>
+);
+export default Terms;
