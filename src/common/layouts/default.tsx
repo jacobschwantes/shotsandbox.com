6e2c8735f216b84d41240cb9b6d0bdbb7fbf013e
skrt-dev <jacobschwantes@outlook.com>
2022-09-02 11:43:14 -0500

    feat: add header tags

--- src/common/layouts/default.tsx
+++ src/common/layouts/default.tsx
@@ -1,9 +1,16 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { Header } from "@components/index";
 import { Footer2 } from "@components/Footer/Footer";
 const Layout: NextPage = ({ children }) => {
   return (
     <div className="flex flex-col font-inter min-h-screen overflow-y-auto dark:bg-black ">
+      <Head>
+        <title>
+          screenshotify
+          </title>
+
+      </Head>
       <Header />
       {children}
       <Footer2 />
