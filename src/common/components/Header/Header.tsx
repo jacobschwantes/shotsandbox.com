51853d0b4dbcedfb88efcb18b40cb317f1993884
skrt-dev <jacobschwantes@outlook.com>
2022-09-06 22:36:09 -0500

    style: colors

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -77,7 +77,7 @@ export default function Header() {
 
               <div className="flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 dark:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 dark:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-600">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
