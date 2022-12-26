984ed99eaf7505c4763566fcff7580aaf3a07312
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2022-12-26 00:19:37 -0600

    feat: create ticker

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -2,4 +2,6 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Announcement from "./Announcement";
 import Tabs from "./Tabs";
-export { Header, Footer, Announcement, Tabs };
+import Carousel from "./Carousel"
+import CTA from "./CTA";
+export { Header, Footer, Announcement, Tabs, Carousel, CTA };
