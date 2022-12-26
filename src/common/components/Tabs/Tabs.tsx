4e37dc3d891bab7da6fa5b960c1b2a02168fd9ce
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2022-12-26 00:19:30 -0600

    style: styling

--- src/common/components/Tabs/Tabs.tsx
+++ src/common/components/Tabs/Tabs.tsx
@@ -13,16 +13,16 @@ const Tabs: NextComponentType<NextPageContext, {}, TabsProps> = ({
   setSelected,
 }) => (
   <div>
-    <div className="flex space-x-2 rounded-xl bg-gray-100 dark:bg-black dark:border dark:border-zinc-900 p-1 max-w-4xl ">
+    <div className="flex space-x-2 rounded-xl bg-gray-100  p-1 max-w-4xl ">
       {tabs.map((tab) => (
         <button
           onClick={() => setSelected(tab)}
           className={clsx(
-            "flex w-full items-center justify-center rounded-lg py-2.5 text-center text-sm font-medium capitalize leading-5 transition-all duration-300 hover:brightness-150",
-            "ring-blue-600  ring-offset-2 ring-offset-blue-400 focus:outline-none dark:ring-offset-black focus:ring-2",
+            "flex w-full items-center justify-center rounded-lg py-2.5 text-center text-sm font-medium capitalize leading-5 transition-all duration-300 select-none outline-none",
+            "ring-blue-600  ring-offset-2 ring-offset-white focus:outline-none focus:ring-2",
             tab === selected
-              ? "bg-white text-blue-700 dark:text-blue-500 shadow dark:bg-blue-900 dark:bg-opacity-30  "
-              : "text-gray-700 hover:text-gray-600 dark:hover:text-zinc-200 dark:text-zinc-300"
+              ? "bg-white text-blue-700  shadow   "
+              : "text-gray-700 hover:text-gray-600 "
           )}
         >
           <h1 className="">{tab}</h1>
