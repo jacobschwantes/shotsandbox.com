bd70b35c1dbfd600b6d9bf4e8c6c124bff7354ee
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2022-12-26 00:25:29 -0600

    refactor: move pricing to index, 404 styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,22 +5,22 @@ import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 export default function Example() {
   return (
-    <div className="flex-1 flex flex-col h-full">
-      <main className="flex-grow mx-auto max-w-7xl w-full flex flex-col px-4 sm:px-6 lg:px-8 mt-16">
-        <div className="flex-shrink-0 my-auto py-16 sm:py-32">
-          <p className="text-sm font-semibold  uppercase tracking-wide text-blue-500">
-            404 error
+    <div className="flex-1 flex flex-col h-full min-h-screen">
+      <main className="flex-grow mx-auto max-w-7xl w-full flex flex-col items-center px-4 sm:px-6 lg:px-8">
+        <div className=" my-auto py-16 sm:py-32 flex flex-col items-center space-y-4">
+          <p className="text-9xl font-bold  uppercase tracking-wide text-blue-600">
+            404
           </p>
-          <h1 className="mt-2 text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl dark:text-zinc-50">
-            Page not found
+          <h1 className="text-6xl font-bold text-gray-900 tracking-tight ">
+            Page not found.
           </h1>
-          <p className="mt-2 text-base text-gray-500 dark:text-zinc-400">
+          <p className=" text-lg text-zinc-600 ">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
-          <div className="mt-6">
+          <div className="">
             <Link href="/">
-              <a className="text-base font-medium text-blue-600">
-                Go back home<span aria-hidden="true"> &rarr;</span>
+              <a className="inline-flex w-full justify-center items-center rounded-lg border border-transparent bg-blue-600  px-4 py-3 font-medium text-lg text-white shadow-sm hover:bg-blue-700 focus:outline-2 focus:outline outline-offset-2 focus:outline-blue-500 transition-colors duration-300">
+                Go back home
               </a>
             </Link>
           </div>
