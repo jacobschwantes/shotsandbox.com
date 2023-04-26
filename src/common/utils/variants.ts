cbe4aa737f10b883499eeea7201a2430f8b09533
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2023-04-25 20:59:23 -0500

    feat: create scaleUp variant

--- src/common/utils/variants.ts
+++ src/common/utils/variants.ts
@@ -65,12 +65,23 @@ export const slideUpSlow = {
   },
 };
 export const fadeIn = {
-  hidden: { opacity: 0},
+  hidden: { opacity: 0 },
   visible: {
     opacity: 1,
     transition: {
-      delay: .25,
-    duration: .5
+      delay: 0.25,
+      duration: 0.5,
+    },
+  },
+};
+export const scaleUp = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.25,
+      duration: 0.4,
     },
   },
 };
