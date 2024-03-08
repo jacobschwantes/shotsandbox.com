3e897ea6237e3076ce802fcb989764f006b49ca3
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2024-03-07 21:41:38 -0600

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import "../common/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@layouts/default";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      <Analytics />
+      {/* <Analytics /> */}
+      <Script
+        defer
+        data-domain="shotsandbox.com"
+        src="http://analytics.endeavorwebsolutions.com/js/script.js"
+      />
     </>
   );
 }
