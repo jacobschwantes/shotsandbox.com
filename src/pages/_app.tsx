d4a49d3ddc5189e1864403754f1235524415e175
Jacob <74641690+jacobschwantes@users.noreply.github.com>
2024-03-08 00:27:08 -0600

    remove unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "../common/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@layouts/default";
 import { Analytics } from "@vercel/analytics/react";
-import PlausibleProvider from "next-plausible";
 import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
